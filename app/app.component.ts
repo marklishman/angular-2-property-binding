d20b5ae0efbbe6a755716a915f695f2e65c02c87
Mark <mark.lishman@gmail.com>
2016-07-02 11:44:06 +0100

    Removed unwanted import

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
 
 import { PropertyComponent } from "./property.component";
 import { ExpressionComponent } from "./expression.component";
